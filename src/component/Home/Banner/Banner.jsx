93d44a23600fcc9dbde5aace2c293870e4a6ad0a
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-09-07 07:41:20 +0600

    text animation apply for banner

--- src/component/Home/Banner/Banner.jsx
+++ src/component/Home/Banner/Banner.jsx
@@ -1,91 +1,10 @@
-// import { useEffect, useState } from 'react';
-// import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 
-// const Banner = () => {
-// 	const slides = [
-// 		{
-// 		  url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
-// 		},
-// 		{
-// 		  url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-// 		},
-// 		{
-// 		  url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
-// 		},
-
-// 		{
-// 		  url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
-// 		},
-// 		{
-// 		  url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
-// 		},
-// 	  ];
-
-// 	  const [currentIndex, setCurrentIndex] = useState(0);
-
-// 	  const prevSlide = () => {
-// 		const isFirstSlide = currentIndex === 0;
-// 		const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-// 		setCurrentIndex(newIndex);
-// 	  };
-
-// 	  const nextSlide = () => {
-// 		const isLastSlide = currentIndex === slides.length - 1;
-// 		const newIndex = isLastSlide ? 0 : currentIndex + 1;
-// 		setCurrentIndex(newIndex);
-// 	  };
-
-// 	  const goToSlide = (slideIndex) => {
-// 		setCurrentIndex(slideIndex);
-// 	  };
-
-// 	  useEffect(() => {
-// 		const interval = setInterval(() => {
-// 		  nextSlide();
-// 		}, 6000); // Changes slide every 3 seconds
-
-// 		return () => clearInterval(interval); // Cleanup interval on component unmount
-// 	  // eslint-disable-next-line react-hooks/exhaustive-deps
-// 	  }, [currentIndex]);
-// 	return (
-// 		<div>
-// 			<div className='max-w-[1400px] h-72  md:h-[780px] w-full m-auto relative group'>
-//       <div
-//         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-//         className='w-full h-full  bg-center bg-cover duration-500'
-//       ></div>
-//       {/* Left Arrow */}
-//       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl  p-2 border-2 hover:bg-white hover:text-black text-white cursor-pointer'>
-//         <BsChevronCompactLeft onClick={prevSlide} size={30} />
-//       </div>
-//       {/* Right Arrow */}
-//       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl  p-2 border-2 hover:bg-white hover:text-black text-white cursor-pointer'>
-//         <BsChevronCompactRight onClick={nextSlide} className='' size={30} />
-//       </div>
-//       <div className='flex top-4 justify-center py-2'>
-//         {slides.map((slide, slideIndex) => (
-//           <div
-//             key={slideIndex}
-//             onClick={() => goToSlide(slideIndex)}
-//             className='text-2xl cursor-pointer'
-//           >
-
-
-//           </div>
-//         ))}
-
-//       </div>
-//     </div>
-// 		</div>
-// 	);
-// };
-
-// export default Banner;
 import { useEffect, useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import houseOne from "../../../assets/Home/1.png"
 import houseTwo from "../../../assets/Home/14.png"
 import houseThree from "../../../assets/Home/2.png"
+import 'animate.css';
 
 const Banner = () => {
 	const slides = [
@@ -148,17 +67,29 @@ const Banner = () => {
 
 				{/* Overlay Text */}
 				<div className="space-y-8  absolute inset-0 hidden md:flex flex-col items-center justify-center text-white text-center px-4">
-					<h2 className=" md:text-xl font-semibold   ">{slides[currentIndex].subtitle}</h2>
-					<h1 className=" md:text-4xl lg:text-5xl font-bold ">{slides[currentIndex].title}</h1>
-					<p className="w-1/2 mx-auto md:text-lg  ">{slides[currentIndex].description}</p>
-					<button className="bg-[#aa8453] text-white hover:bg-[#2e2314] hover:text-[#aa8453] px-9 py-4 rounded-full transform transition duration-400 ease-in-out">
+					<h2
+						key={`subtitle-${currentIndex}`}
+
+						className="md:text-xl animate__delay-1s animate__animated animate__fadeInUp font-sen">
+						{slides[currentIndex].subtitle}
+					</h2>
+					<h1
+						key={`title-${currentIndex}`}
+
+						className="animate__delay-2s animate__animated animate__fadeInUp  md:text-4xl lg:text-5xl font-sen font-bold ">{slides[currentIndex].title}</h1>
+					<p
+						key={`description-${currentIndex}`}
+						className="animate__delay-3s animate__animated animate__fadeInUp w-1/2 mx-auto md:text-lg font-barlow ">{slides[currentIndex].description}</p>
+					<button 
+						key={`button-${currentIndex}`}
+					className="animate__delay-3s animate__animated animate__fadeInUp bg-[#aa8453] font-barlow text-white hover:bg-[#2e2314] hover:text-[#aa8453] px-9 py-4 rounded-full transform transition duration-400 ease-in-out">
 						Preview Listing
 					</button>
 
 				</div>
 
 				{/* search card */}
-				<div className="relative -mt-48 z-10">
+				<div className="relative md:-mt-48 z-10">
 					<div className="bg-white shadow-lg p-7 w-[90%] mx-auto">
 						<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4">
 							{/* Enter Keyword Input */}
--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,179 +1,3 @@
-// // import { IoIosCall } from "react-icons/io";
-// // import { Link } from "react-router-dom";
-// // import logo from "../../assets/logo-white.png"
-// // import { HiOutlineMenuAlt3 } from "react-icons/hi";
-// // import { useState } from "react";
-
-// // const Navbar = () => {
-// // 	const [isOpen, setIsOpen] = useState(false);
-
-// // 	const toggleOffcanvas = () => {
-// // 	  setIsOpen(!isOpen);
-// // 	};
-
-// // 	return (
-// // 		<>
-
-// // 			<div className=" text-white md:px-8 lg:px-16  bg-[#222222]">
-
-// // 			{/* frist nav */}
-// // 			<nav className="hidden md:flex justify-between  pt-[10px] font-barlow text-[13px]">
-// // 				<div className="flex items-center">
-// // 					<IoIosCall className="text-lg "/>
-// // 					<p > Need Support ? +1-435-782-4312</p>
-// // 				</div>
-// // 				<div> 
-// // 					<ul className="flex space-x-4">
-// // 						<li><Link to="#">About</Link></li>
-// // 						<li><Link to="#">Sign Up</Link></li>
-// // 						<li><Link to="#">Login</Link></li>
-// // 					</ul>
-// // 				</div>
-// // 			</nav>
-
-// // 			{/* second nav */}
-// // 			<nav className="flex items-center justify-between pt-4">
-// // 				{/* logo */}
-// // 				<div>
-// // 					<img src={logo} alt="" />
-// // 				</div>
-
-// // 				{/* nav item and button */}
-// // 				<div className="">
-// // 					{/* nav item for large device */}
-// // 					<ul className="lg:flex hidden gap-7 items-center">
-
-// // 						<li><Link to = "#">Home +</Link></li>
-// // 						<li><Link to = "#">Listing +</Link></li>
-// // 						<li><Link to = "#">Features +</Link></li>
-// // 						<li><Link to = "#">Pages +</Link></li>
-// // 						<li><Link to = "#">Blog +</Link></li>
-// // 						<li><Link to = "#">Contact</Link></li>
-
-// // 					<button className="bg-[#aa8453] px-[22px] py-[10px] font-sen text-base">+ Create Listing</button>
-// // 					</ul>
-
-// // 					{/* nav for small device */}
-// // 					<HiOutlineMenuAlt3 className="text-4xl lg:hidden" />
-
-
-
-
-
-
-
-
-// // 				</div>
-// // 			</nav>
-// // 			</div>
-
-
-// // 		</>
-// // 	);
-// // };
-
-// // export default Navbar;
-
-
-
-// import { IoIosCall } from "react-icons/io";
-// import { Link } from "react-router-dom";
-// import logo from "../../assets/logo-white.png";
-// import { HiOutlineMenuAlt3 } from "react-icons/hi";
-// import { useState } from "react";
-
-// const Navbar = () => {
-//   const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(false);
-
-//   const toggleOffcanvas = () => {
-//     setIsOffcanvasOpen(!isOffcanvasOpen);
-//   };
-
-//   return (
-//     <>
-//       <div className="pb-20 text-white md:px-8 lg:px-16 bg-[#222222]">
-//         {/* First nav */}
-//         <nav className="hidden md:flex justify-between pt-[10px] font-barlow text-[13px]">
-//           <div className="flex items-center">
-//             <IoIosCall className="text-lg" />
-//             <p>Need Support? +1-435-782-4312</p>
-//           </div>
-//           <div>
-//             <ul className="flex space-x-4">
-//               <li><Link to="#">About</Link></li>
-//               <li><Link to="#">Sign Up</Link></li>
-//               <li><Link to="#">Login</Link></li>
-//             </ul>
-//           </div>
-//         </nav>
-
-//         {/* Second nav */}
-//         <nav className="flex items-center justify-between pt-4">
-//           {/* Logo */}
-//           <div>
-//             <img src={logo} alt="Logo" />
-//           </div>
-
-//           {/* Nav items for large devices */}
-//           <div className="lg:flex hidden gap-7 items-center">
-//             <ul className="flex gap-7 items-center">
-//               <li><Link to="#">Home +</Link></li>
-//               <li><Link to="#">Listing +</Link></li>
-//               <li><Link to="#">Features +</Link></li>
-//               <li><Link to="#">Pages +</Link></li>
-//               <li><Link to="#">Blog +</Link></li>
-//               <li><Link to="#">Contact</Link></li>
-//             </ul>
-//             <button className="bg-[#aa8453] px-[22px] py-[10px] font-sen text-base">
-//               + Create Listing
-//             </button>
-//           </div>
-
-//           {/* Offcanvas menu for small devices */}
-//           <HiOutlineMenuAlt3
-//             className="text-4xl lg:hidden cursor-pointer"
-//             onClick={toggleOffcanvas}
-//           />
-
-//           {/* Offcanvas Top */}
-//           <div
-//             className={`fixed top-0 left-0 w-full  bg-gray-800 text-white transform transition-transform ${
-//               isOffcanvasOpen ? "translate-y-0" : "-translate-y-full"
-//             }`}
-//             style={{ zIndex: 1000 }}
-//           >
-//             {/* Offcanvas header */}
-//             <div className="flex justify-between items-center p-4">
-//               <h5 className="text-lg">Offcanvas Top</h5>
-//               <button
-//                 type="button"
-//                 className="text-white text-2xl"
-//                 onClick={toggleOffcanvas}
-//               >
-//                 &times;
-//               </button>
-//             </div>
-
-//             {/* Offcanvas body */}
-//             <div className="p-4">
-//               <ul>
-//                 <li><Link to="#">Home +</Link></li>
-//                 <li><Link to="#">Listing +</Link></li>
-//                 <li><Link to="#">Features +</Link></li>
-//                 <li><Link to="#">Pages +</Link></li>
-//                 <li><Link to="#">Blog +</Link></li>
-//                 <li><Link to="#">Contact</Link></li>
-
-//               </ul>
-//             </div>
-//           </div>
-//         </nav>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Navbar;
 
 import { IoIosCall } from "react-icons/io";
 import { Link } from "react-router-dom";
@@ -189,7 +13,7 @@ const Navbar = () => {
   
   const [isScrolled, setIsScrolled] = useState(false);
   const handleScroll = () => {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 120) {
       setIsScrolled(true);  // If scrolled down 50px or more, add background
     } else {
       setIsScrolled(false);  // If near the top, remove background
@@ -212,19 +36,7 @@ const Navbar = () => {
             : ' fixed top-0 text-white left-0 w-full z-50'
         }`}>
         {/* First nav */}
-        {/* <nav className="hidden md:flex justify-between pt-[10px] font-barlow text-[13px]">
-          <div className="flex items-center">
-            <IoIosCall className="text-lg" />
-            <p>Need Support? +1-435-782-4312</p>
-          </div>
-          <div>
-            <ul className="flex space-x-4">
-              <li><Link to="#">About</Link></li>
-              <li><Link to="#">Sign Up</Link></li>
-              <li><Link to="#">Login</Link></li>
-            </ul>
-          </div>
-        </nav> */}
+      
         <nav
   className={` transition-all duration-500 ease-in-out ${
     isScrolled ? "hidden" : "md:flex hidden"
@@ -251,30 +63,30 @@ const Navbar = () => {
 
 
         {/* Second nav */}
-        <nav className="flex items-center justify-between pt-4">
+        <nav className="flex items-center justify-between pt-2  md:pt-4">
           {/* Logo */}
           <div>
-            <img src={logo} alt="Logo" />
+            <img src={logo} className="w-28 md:w-36" alt="Logo" />
           </div>
 
           {/* Nav items for large devices */}
           <div className="lg:flex hidden gap-7 items-center">
-            <ul className="flex gap-7 items-center">
-              <li><Link to="#">Home +</Link></li>
-              <li><Link to="#">Listing +</Link></li>
-              <li><Link to="#">Features +</Link></li>
-              <li><Link to="#">Pages +</Link></li>
-              <li><Link to="#">Blog +</Link></li>
-              <li><Link to="#">Contact</Link></li>
+            <ul className="flex gap-7 items-center font-sen">
+              <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">Home +</Link></li>
+              <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">Listing +</Link></li>
+              <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">Features +</Link></li>
+              <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">Pages +</Link></li>
+              <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">Blog +</Link></li>
+              <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">Contact</Link></li>
             </ul>
-            <button className="bg-[#aa8453] px-[22px] py-[10px] font-sen text-base">
+            <button className="bg-[#aa8453] hover:bg-[#b39063] px-[22px] py-[10px] font-sen text-base">
               + Create Listing
             </button>
           </div>
 
           {/* Offcanvas menu for small devices */}
           <HiOutlineMenuAlt3
-            className="text-4xl lg:hidden cursor-pointer"
+            className="text-4xl text-[#aa8453] lg:hidden cursor-pointer"
             onClick={toggleOffcanvas}
           />
 
@@ -286,37 +98,37 @@ const Navbar = () => {
   {/* Offcanvas body */}
   <div className="p-4 overflow-y-auto h-full">
     <ul className="space-y-4">
-      <li><Link to="#">
+      <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">
         <div className="flex justify-between">
           <p>Home</p>
           <p>+</p>
         </div>
       </Link></li>
-      <li><Link to="#">
+      <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">
         <div className="flex justify-between">
           <p>Listing</p>
           <p>+</p>
         </div>
       </Link></li>
-      <li><Link to="#">
+      <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">
         <div className="flex justify-between">
           <p>Features</p>
           <p>+</p>
         </div>
       </Link></li>
-      <li><Link to="#">
+      <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">
         <div className="flex justify-between">
           <p>Pages</p>
           <p>+</p>
         </div>
       </Link></li>
-      <li><Link to="#">
+      <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">
       <div className="flex justify-between">
           <p>Blogs</p> 
         <p>+</p>
         </div>
       </Link></li>
-      <li><Link to="#">Contact</Link></li>
+      <li className="hover:text-[#aa8453] transform transition duration-200 ease-out"><Link to="#">Contact</Link></li>
     </ul>
     <button className="bg-[#aa8453] px-[22px] py-[10px] font-sen text-base mt-4">
       + Create Listing
