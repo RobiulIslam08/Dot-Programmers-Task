6a4a4e584aff257beba456102e17dcb64f67ca30
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-09-07 21:38:48 +0600

    category card design done

--- src/component/Home/PropertyCategories/PropertyCategories.jsx
+++ src/component/Home/PropertyCategories/PropertyCategories.jsx
@@ -1,3 +1,10 @@
+import { BsShop } from "react-icons/bs";
+import { FaWarehouse } from "react-icons/fa";
+import { FaHouseFloodWater } from "react-icons/fa6";
+import { GoHome } from "react-icons/go";
+import { LiaLandmarkSolid } from "react-icons/lia";
+import { MdBedroomChild, MdOtherHouses } from "react-icons/md";
+import { PiBuildingApartmentLight, PiBuildingOffice } from "react-icons/pi";
 
 
 const PropertyCategories = () => {
@@ -11,6 +18,61 @@ const PropertyCategories = () => {
 			</div>
 
 			{/* category */}
+			<div className="grid grid-cols-1 md:grid-cols-4 pt-10  lg:grid-cols-5  gap-3 ">
+				<div className="bg-white px-3  xl:px-8 py-6 flex flex-col justify-center items-center ">
+					<GoHome className="text-[#aa8453] text-[53px] mb-3"/>
+					<h1 className="font-sen text-lg font-bold pb-3">Livign House</h1>
+					<p className="text-center text-[#6d7175]">Preview Listing of accommdation property living houses</p>
+				</div>
+				<div className="bg-white px-3  xl:px-8 py-6 flex flex-col justify-center items-center ">
+					<LiaLandmarkSolid className="text-[#aa8453] text-[53px] mb-3"/>
+					<h1 className="font-sen text-lg font-bold pb-3">Solid Land</h1>
+					<p className="text-center text-[#6d7175]">Hight listed solid use-able land most popular area for development</p>
+				</div>
+				<div className="bg-white px-3  xl:px-8 py-6 flex flex-col justify-center items-center ">
+					<PiBuildingOffice className="text-[#aa8453] text-[53px] mb-3"/>
+					<h1 className="font-sen text-lg font-bold pb-3">Office Floor</h1>
+					<p className="text-center text-[#6d7175]">Preview most popular area office building listed category</p>
+				</div>
+				<div className="bg-white px-3  xl:px-8 py-6 flex flex-col justify-center items-center ">
+					<BsShop className="text-[#aa8453] text-[53px] mb-3"/>
+					<h1 className="font-sen text-lg font-bold pb-3">Commercial</h1>
+					<p className="text-center text-[#6d7175]">Listing Commercila property for buisness and factory development</p>
+				</div>
+				<div className="bg-white px-3  xl:px-8 py-6 flex flex-col justify-center items-center ">
+					<MdBedroomChild className="text-[#aa8453] text-[53px] mb-3"/>
+					<h1 className="font-sen text-lg font-bold pb-3">Hostel Room</h1>
+					<p className="text-center text-[#6d7175]">If you are single looking for single living, preview the hostel listing</p>
+				</div>
+				<div className="bg-white px-3  xl:px-8 py-6 flex flex-col justify-center items-center ">
+					<PiBuildingApartmentLight className="text-[#aa8453] text-[53px] mb-3"/>
+					<h1 className="font-sen text-lg font-bold pb-3">Appartment</h1>
+					<p className="text-center text-[#6d7175]">For family living appartment, find your best appartment in our director list</p>
+				</div>
+				<div className="bg-white px-3  xl:px-8 py-6 flex flex-col justify-center items-center ">
+					<FaWarehouse className="text-[#aa8453] text-[53px] mb-3"/>
+					<h1 className="font-sen text-lg font-bold pb-3">Condo</h1>
+					<p className="text-center text-[#6d7175]">In our directory we have listed luxury condo for rent and sale</p>
+				</div>
+				<div className="bg-white px-3  xl:px-8 py-6 flex flex-col justify-center items-center ">
+					<FaHouseFloodWater className="text-[#aa8453] text-[53px] mb-3"/>
+					<h1 className="font-sen text-lg font-bold pb-3">Villa House</h1>
+					<p className="text-center text-[#6d7175]">Please who like villa house click here this category listed</p>
+				</div>
+				<div className="bg-white px-3  xl:px-8 py-6 flex flex-col justify-center items-center ">
+					<MdOtherHouses className="text-[#aa8453] text-[53px] mb-3"/>
+					<h1 className="font-sen text-lg font-bold pb-3">Hostel Room</h1>
+					<p className="text-center text-[#6d7175]">The Traveller people can find best hotel here for living night</p>
+				</div>
+				
+			</div>
+
+
+		
+
+			
+
+
 		</div>
 	);
 };
