351f2a4c3da447163c49f3dee72809f99eba41ad
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-09-08 09:03:42 +0600

    client testimonial design done

--- src/component/Home/ClientTestimonials/ClientTestimonials.jsx
+++ src/component/Home/ClientTestimonials/ClientTestimonials.jsx
@@ -1,4 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+import { TfiQuoteRight } from 'react-icons/tfi';
 
 const ClientTestimonials = () => {
 	return (
@@ -8,7 +19,43 @@ const ClientTestimonials = () => {
 				<p className="text-lg font-barlow text-[#b28f62] pb-4">Testimonial</p>
 				<h1 className="font-sen font-bold text-4xl">Whay Client Says About Us</h1>
 				<div className="p-[2px] w-10 my-6  mx-auto  bg-[#aa8453]"></div>
+			</div>
 
+			{/* testimonial slider */}
+			<div className='w-full px-10 md:w-[630px] lg:w-[490px] xl:w-[640px] mx-auto text-center pb-20'>
+				<Swiper
+					pagination={{
+						dynamicBullets: true,
+					}}
+					modules={[Pagination]}
+					className="mySwiper"
+				>
+					<SwiperSlide>
+						<div className='flex flex-col justify-center mb-10 items-center'>
+							<TfiQuoteRight className='text-[#aa8453] text-5xl mt-7 '></TfiQuoteRight>
+							<p className='font-barlow text-[20px] text-[#414141] py-7'>"Posuere mu scurabitur parturient scelerisque suspendissse elementum facilissi dignissim nan purus torquent turpis interdum hendrerit erat ultrices pretium risus elementum. Fringilla alliquet aliquet mattis lacinia quam a montes maecenas parturient urna varis. Sollicitudin pede sapien taciti dui."</p>
+							<h1 className='text-[#222222] font-sen pb-2 text-[24px]'>Mark Wiggins</h1>
+							<p className='text-[#ccb597] font-barlow'>CEO ( Grodins Group )</p>
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div className='flex flex-col justify-center items-center'>
+							<TfiQuoteRight className='text-[#aa8453] text-5xl mt-7 '></TfiQuoteRight>
+							<p className='font-barlow text-[20px] text-[#414141] py-7'>"Posuere mu scurabitur parturient scelerisque suspendissse elementum facilissi dignissim nan purus torquent turpis interdum hendrerit erat ultrices pretium risus elementum. Fringilla alliquet aliquet mattis lacinia quam a montes maecenas parturient urna varis. Sollicitudin pede sapien taciti dui."</p>
+							<h1 className='text-[#222222] font-sen pb-2 text-[24px]'>Mark Wiggins</h1>
+							<p className='text-[#ccb597] font-barlow'>CEO ( Grodins Group )</p>
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div className='flex flex-col justify-center items-center'>
+							<TfiQuoteRight className='text-[#aa8453] text-5xl mt-7 '></TfiQuoteRight>
+							<p className='font-barlow text-[20px] text-[#414141] py-7'>"Posuere mu scurabitur parturient scelerisque suspendissse elementum facilissi dignissim nan purus torquent turpis interdum hendrerit erat ultrices pretium risus elementum. Fringilla alliquet aliquet mattis lacinia quam a montes maecenas parturient urna varis. Sollicitudin pede sapien taciti dui."</p>
+							<h1 className='text-[#222222] font-sen pb-2 text-[24px]'>Mark Wiggins</h1>
+							<p className='text-[#ccb597] font-barlow'>CEO ( Grodins Group )</p>
+						</div>
+					</SwiperSlide>
+
+				</Swiper>
 			</div>
 		</div>
 	);
